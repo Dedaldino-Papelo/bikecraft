7db384b8050360cf92efe3720fbbd28418fd01b9
Dedalino <kelsonpapelo18@gmail.com>
2022-08-16 22:07:38 +0100

    melhorando a questão do SEO do site com a implementação de tags no Head

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@ $(".rslides").responsiveSlides({
     setTimeout(function () {
       $(".animar").addClass("animated fadeInDown");
     }, 1600);
+    
     setTimeout(function () {
       $(".introducao-interna h1").addClass("animated fadeInDown");
     }, 400);
@@ -27,3 +28,57 @@ $(".rslides").responsiveSlides({
       $(".animar-interno").addClass("animated fadeInDown");
     }, 1200);
   });
+
+  // Formulario
+
+$('.formphp').on('submit', function() {
+	var emailContato = "contato@bikcraft.com"; // Escreva aqui o seu e-mail
+
+	var that = $(this),
+			url = that.attr('action'),
+			type = that.attr('method'),
+			data = {};
+	
+	that.find('[name]').each(function(index, value) {
+		var that = $(this),
+				name = that.attr('name'),
+				value = that.val();
+				
+		data[name] = value;
+	});
+	
+	$.ajax({
+		url: url,
+		type: type,
+		data: data,
+		success: function(response) {
+		
+			if( $('[name="leaveblank"]').val().length != 0 ) {
+				$('.formphp').html("<div id='form-erro'></div>");
+				$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
+				.hide()
+				.fadeIn(1500, function() {
+				$('#form-erro');
+				});
+			} else {
+			
+				$('.formphp').html("<div id='form-send'></div>");
+				$('#form-send').html("<span>Mensagem enviada!</span><p>Em breve eu entro em contato com você. Abraços.</p>")
+				.hide()
+				.fadeIn(1500, function() {
+				$('#form-send');
+				});
+			};
+		},
+		error: function(response) {
+			$('.formphp').html("<div id='form-erro'></div>");
+			$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
+			.hide()
+			.fadeIn(1500, function() {
+			$('#form-erro');  
+		});
+		}
+	});
+	
+	return false;
+});
