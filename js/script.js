3f6403955879f9cb6c48640d453314315d41b510
Dedalino <kelsonpapelo18@gmail.com>
2022-08-16 20:16:30 +0100

    adding animation

--- js/script.js
+++ js/script.js
@@ -3,3 +3,27 @@ $(".rslides").responsiveSlides({
     speed: 500,            // Integer: Speed of the transition, in milliseconds
     timeout: 3000,          // Integer: Time between slide transitions, in milliseconds
   });
+
+  Visibility.onVisible(function(){
+    setTimeout(function () {
+      $(".introducao h1").addClass("animated fadeInDown");
+    }, 400);
+    setTimeout(function () {
+      $(".introducao blockquote").addClass("animated fadeInDown");
+    }, 800);
+    setTimeout(function () {
+      $(".introducao .btn").addClass("animated fadeInDown");
+    }, 1200);
+    setTimeout(function () {
+      $(".animar").addClass("animated fadeInDown");
+    }, 1600);
+    setTimeout(function () {
+      $(".introducao-interna h1").addClass("animated fadeInDown");
+    }, 400);
+    setTimeout(function () {
+      $(".introducao-interna p").addClass("animated fadeInDown");
+    }, 800);
+    setTimeout(function () {
+      $(".animar-interno").addClass("animated fadeInDown");
+    }, 1200);
+  });
